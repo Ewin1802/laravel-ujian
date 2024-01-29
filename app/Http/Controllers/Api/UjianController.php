7cb24c8d4ec1d90ee8b6a6bfa3afa512673ae8b5
Ewin1802 <elantapa@gmail.com>
2024-01-29 23:58:17 +0800

    2

--- app/Http/Controllers/Api/UjianController.php
+++ app/Http/Controllers/Api/UjianController.php
@@ -248,7 +248,7 @@ class UjianController extends Controller
             ]);
         });
 
-        dd($ujian);
+        //dd($ujian);
 
         return response()->json([
             'message' => 'Berhasil mendapatkan hasil',
